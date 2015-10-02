31eb3c49b73adb62b08ff16395c4ebb994dd38cc
Nikita Grishko <grin.minsk@gmail.com>
2015-10-02 22:38:54 +0300

    Work on authentication using GitHub (initialize GitHub using settings)

--- app/modules/oauth2/github.go
+++ app/modules/oauth2/github.go
@@ -1,15 +1,34 @@
 package oauth2
 
+import (
+	"strings"
+
+	"github.com/revel/revel"
+)
+
 var (
+	GitHub *Config
+)
+
+func init() {
+	revel.OnAppStart(initGitHub)
+}
+
+func initGitHub() {
+	clientID, _ := revel.Config.String("auth.github.client_id")
+	clientSecret, _ := revel.Config.String("auth.github.client_secret")
+	redirectUrl, _ := revel.Config.String("auth.github.redirect_url")
+	scopes, _ := revel.Config.String("auth.github.scopes")
+
 	GitHub = &Config{
-		ClientID:     "fake",
-		ClientSecret: "fake",
-		RedirectUrl:  "http://localhost",
-		Scopes:       []string{},
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectUrl:  redirectUrl,
+		Scopes:       strings.Split(scopes, ","),
 
 		Endpoint: Endpoint{
 			AuthUrl:  "https://github.com/login/oauth/authorize",
 			TokenUrl: "https://github.com/login/oauth/access_token",
 		},
 	}
-)
+}
